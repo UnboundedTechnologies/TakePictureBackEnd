418a371f307802fe679df632f44101a3a30ac6d3
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2023-03-01 00:46:19 +0100

    Added Render + Cloudinary deployment

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cfv8jf02qv24oq2v4ao0-a.frankfurt-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'takepicture'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', '7GB6pocDPyLrKT3GPB0vQAZOIwtu6BIE'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      schema: process.env.DATABASE_SCHEMA, // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
