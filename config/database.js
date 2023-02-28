7f8dc66e898695eb7b546bad00d6b8f862a5ca39
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2023-03-01 00:34:53 +0100

    Added Render + Cloudinary deployment

--- config/database.js
+++ config/database.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'dpg-cfv8jf02qv24oq2v4ao0-a.frankfurt-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'takepicture'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', '7GB6pocDPyLrKT3GPB0vQAZOIwtu6BIE'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -9,4 +9,19 @@ module.exports = ({ env }) => ({
       roles: ["strapi-super-admin"]
     },
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
