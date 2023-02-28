c78f6e864f50a2116b2b4cb6b80d8867e0418b9f
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2023-02-28 23:32:34 +0100

    Redeploy to avoid 404 on Vercel.

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
       apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
       appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+
       roles: ["strapi-super-admin"]
     },
   },
