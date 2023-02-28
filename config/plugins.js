7507d1f127ff8d3413b5f239d7073bb741dc978c
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2023-02-28 23:23:35 +0100

    Start vercel config

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      roles: ["strapi-super-admin"]
+    },
+  },
+});
